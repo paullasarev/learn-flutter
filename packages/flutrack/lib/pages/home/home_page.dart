5a60d4e6aad2ef0f38b567c6af1ec96f35dc359f
Pavel_Lasarev <paullasarev@gmail.com>
2021-06-17 15:50:09 +0400

    use provider to store bloc

--- packages/flutrack/lib/pages/home/home_page.dart
+++ packages/flutrack/lib/pages/home/home_page.dart
@@ -2,22 +2,24 @@ import 'package:flutrack/hooks/use_bloc_state.dart';
 import 'package:flutrack/pages/home/home_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:provider/provider.dart';
 
 import 'home_body.dart';
 
 class HomePage extends HookWidget {
   final String title;
-  final _bloc = CounterCubit(0);
+  // final _bloc = CounterCubit(0);
 
   HomePage({Key? key, required this.title}) : super(key: key);
 
-  void _incrementCounter() {
-    // _counter.value++;
-    _bloc.increment();
-  }
-
   @override
   Widget build(BuildContext context) {
+    final _bloc = Provider.of<CounterCubit>(context);
+    void _incrementCounter() {
+      // _counter.value++;
+      _bloc.increment();
+    }
+
     // final _counter = useState<int>(0);
     final _counter = useBlocState(_bloc);
     // void _incrementCounter() {
